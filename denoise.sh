1cc64b5c411ef91c63e2a912aa7ee9b2dee0598c
Elizabeth Nemeti <enemeti@emory.edu>
2026-07-29 16:23:33 -0400

    Create denoise.sh

---
+++ denoise.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=denoise
+#SBATCH --output=LOGS/%x_%j.out
+#SBATCH --error=LOGS/%x_%j.err
+#SBATCH --time=8:00:00
+#SBATCH --mem=64G
+#SBATCH --partition=batch
+#SBATCH --cpus-per-task=1
+
+cd /scripts/directory
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Python script
+python denoise.py
+
